743bc01624a744f8c332ff2329681fddbb426c7a
SJ Choi <sjchoi@mirtech.co.kr>
2025-12-16 17:50:04 +0900

    파일업로드 관련 수정

--- src/main/java/forjun/web/module/system/api/FileUploadController.java
+++ src/main/java/forjun/web/module/system/api/FileUploadController.java
@@ -3,21 +3,30 @@ package forjun.web.module.system.api;
 import forjun.web.module.system.api.dto.UploadedFileResponse;
 import forjun.web.module.system.api.mapper.FileUploadApiMapper;
 import forjun.web.module.system.application.port.in.UploadUsecase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "파일 업로드 API", description = "파일 업로드 API")
 @RestController
 @RequestMapping("/api/system/files")
 @RequiredArgsConstructor
 public class FileUploadController {
 
+    // UseCase
     private final UploadUsecase uploadUsecase;
 
+    // Mapper
     private final FileUploadApiMapper fileUploadApiMapper;
 
+    // 파일 업로드
     @PostMapping()
+    @Operation(summary = "파일 업로드", description = "파일 업로드")
+    @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<UploadedFileResponse> upload(
             @RequestParam("file") MultipartFile file) {
 
